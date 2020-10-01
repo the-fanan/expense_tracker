81e43c14f90c3c551187abb2f10b61615759e5c6
Fanan Dala <fanan.dala@yahoo.com>
2020-10-01 11:51:40 +0100

    styling borders

--- lib/transaction_list_item.dart
+++ lib/transaction_list_item.dart
@@ -17,9 +17,18 @@ class TransactionListItem extends StatelessWidget {
         child: Row(
           children: <Widget>[
             Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 2,
+                ),
+              ),
               width: MediaQuery.of(context).size.width * 0.3,
+              margin: EdgeInsets.all(10),
               padding: EdgeInsets.all(10),
-              child: Text(this.transaction.amount.toString(), style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              child: Text(this.transaction.amount.toString(), style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue),
+              ),
             ),
             Container(
               width: MediaQuery.of(context).size.width * 0.5,
@@ -27,8 +36,8 @@ class TransactionListItem extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(this.transaction.title),
-                  Text(this.transaction.date.toString()),
+                  Text(this.transaction.title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,)),
+                  Text(this.transaction.date.toString(), style: TextStyle(fontSize: 14, color: Colors.grey)),
                 ]
               )
             ),
